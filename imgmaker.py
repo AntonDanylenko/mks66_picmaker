7f31e0449c8103c1e5f74d920d052632256a0282
AntonDanylenko <adanylenko@stuy.edu>
2019-02-01 15:43:16 -0500

    s

--- imgmaker.py
+++ imgmaker.py
@@ -12,47 +12,49 @@ map = [[[0] * 3 for i in range(500)]for i in range(500)]
 
 def body(file):
     image=" "
-    r=127
+    r=0
     g=127
-    b=127
-    i=0
-    while i<500:
-        ii=0
-        g=127
-        while ii<500:
-            if i==0:
-                g=127
+    b=255
+    rise = 1
+    x=0
+    while x<500:
+        rise += random.randint(-1,1)
+        if rise<0:
+            rise=0
+        elif rise>10:
+            rise=10
+        increase = random.randint(-2,rise)
+        y=0
+        while y<500:
+            if y==0:
+                if x==0:
+                    g = 127
+                else:
+                    g = map[x][y-1][1] + random.randint(-1,1)
             else:
-                g = map[i][ii-1][1] + random.randint(random.randint(-5,-1),random.randint(1,7))
+                g = map[x-1][y][1] + increase
+            print("g: " + str(g))
             if g<0:
                 g=0
             elif g>255:
                 g=255
-            if i==0:
-                b=127
-            else:
-                b = map[i][ii-1][2] + random.randint(random.randint(-5,-1),random.randint(1,7))
-            if b<0:
-                b=0
-            elif b>255:
-                b=255
-            map[i][ii][0] = r
-            map[i][ii][1] = g
-            map[i][ii][2] = b
-            ii+=1
-        i+=1
+            map[x][y][0] = r
+            map[x][y][1] = g
+            map[x][y][2] = b
+            y+=1
+        x+=1
     draw(file)
 
 def draw(file):
     image=" "
-    i=0
-    while i<500:
-        ii=0
-        while ii<500:
+    y=0
+    while y<500:
+        x=0
+        while x<500:
             #print(str(map[i][ii][0]) + " " + str(map[i][ii][1]) + " " + str(map[i][ii][2]) + " ")
-            image+=str(map[i][ii][0]) + " " + str(map[i][ii][1]) + " " + str(map[i][ii][2]) + " "
-            ii+=1
-        i+=1
+            image+=str(map[x][y][0]) + " " + str(map[x][y][1]) + " " + str(map[x][y][2]) + " "
+            x+=1
+        y+=1
     file.write(image)
 
 main()
